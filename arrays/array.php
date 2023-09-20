3f99313efbf55c7b27f1bd2a1818ad1802882589
Tiësto <9019545@student.zadkine.nl>
2023-09-20 09:57:16 +0200

    commit

---
+++ arrays/array.php
@@ -0,0 +1,46 @@
+<?php
+
+$persons = ["Timo", "René", "Donny", "Tiësto"];
+foreach($persons as $person){
+    ?>
+    <p><?= $person ?></p>
+    <?php
+}
+
+$animals =["wasbeer","schaap","kip","paard"];
+foreach($animals as $animal){
+    ?>
+    <p><?= $animal ?></p>
+    <?php
+}
+// $languages = [
+//     ["HTML","CSS", "Javascript"],
+//     ["Python", "PHP", "java"],
+//     ["C#", "Unity","JSON"]
+// ];
+// foreach($languages as $language){
+//     foreach($language as $list){
+//        ?>
+//        <p><?= $list ?></p>
+//        <?php
+//     }
+// }
+$people =["Jasper" => 16, "Tiësto" => 18, "RENE" => 17, "donny" => 12, "Timo" => 21];
+foreach($people as $name => $age){
+    echo "mijn naam is ". $name . " en ik ben ".  $age . " jaar oud. ";
+};
+
+$brands=["Albert heijn", "Jumbo", "Dirk", "Plus", "Aldi"];
+?>
+<h1><?= $brands[2]?></h1>
+<?php
+$languages = [
+    ["HTML","CSS"],
+    ["Python","PHP"],
+    ["C#", "Unity"],
+];
+foreach($languages[0] as $language){
+      echo $language ." is een frontend taal";
+    };
+
+?>
